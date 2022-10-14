51710b413c7f3f4506d52da63a8cca353f94f0a9
Galitsky Dmytro <melles.android@gmail.com>
2022-10-14 23:50:11 +0300

    onclite: Update device specific fstab

--- recovery/root/system/bin/merge-fstab.sh
+++ recovery/root/system/bin/merge-fstab.sh
@@ -2,8 +2,3 @@
 
 echo >> /system/etc/recovery.fstab
 cat /system/etc/recovery.fstab.device >> /system/etc/recovery.fstab
-echo >> /system/etc/twrp.flags
-cat /system/etc/twrp.flags.device >> /system/etc/twrp.flags
-
-echo >> /system/etc/twrp.flags
-cat /system/etc/twrp.flags.nondynpart >> /system/etc/twrp.flags
