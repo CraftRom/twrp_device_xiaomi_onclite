e0b182eb78a11bf2b44414cf3161aed8a93e2472
Galitsky Dmytro <melles.android@gmail.com>
2022-10-26 13:11:19 +0300

    fox: vendorsetup.sh: Overhaul script

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -17,78 +17,117 @@
 #
 #  Please maintain this if you use this script or any part of it
 #
-export LC_ALL="C"
-export ALLOW_MISSING_DEPENDENCIES=true
-export OF_TARGET_DEVICES="onclite"
-export TARGET_DEVICE_ALT="onclite, onc"
+
+FDEVICE="onclite"
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
+   if [ -n "$chkdev" ]; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+fox_get_target_device() {
+local chkdev=$(echo "$BASH_SOURCE" | grep $FDEVICE)
+   if [ -n "$chkdev" ]; then
+      FOX_BUILD_DEVICE="$FDEVICE"
+   else
+      chkdev=$(set | grep BASH_ARGV | grep $FDEVICE)
+      [ -n "$chkdev" ] && FOX_BUILD_DEVICE="$FDEVICE"
+   fi
+}
+
+if [ -z "$1" -a -z "$FOX_BUILD_DEVICE" ]; then
+   fox_get_target_device
+fi
+
+if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
+	export LC_ALL="C"
+	export ALLOW_MISSING_DEPENDENCIES=true
+	export OF_TARGET_DEVICES=onclite
+	export TARGET_DEVICE_ALT=onclite,onc
 
 
-# Device Tree Path | Include Path
-export DEVICE_PATH="device/xiaomi/onclite"
-export INCLUDE_PATH="$DEVICE_PATH/include"
+	# Device Tree Path | Include Path
+	export DEVICE_PATH=device/xiaomi/onclite
+	export INCLUDE_PATH=$DEVICE_PATH/include
 
-export OF_MAINTAINER_AVATAR=$INCLUDE_PATH/melles1991.png
+	export OF_MAINTAINER_AVATAR=$INCLUDE_PATH/melles1991.png
 
-#Language
-export TW_DEFAULT_LANGUAGE="en"
+	#Language
+	export TW_DEFAULT_LANGUAGE="en"
 
 
-# Fox-specific flags
-export OF_DONT_PATCH_ENCRYPTED_DEVICE="1"
-export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER="1"
-export OF_NO_TREBLE_COMPATIBILITY_CHECK="1"
-export FOX_USE_BASH_SHELL="1"
-export FOX_ASH_IS_BASH="1"
-export FOX_USE_NANO_EDITOR="1"
-export FOX_USE_TAR_BINARY="1"
-export FOX_USE_ZIP_BINARY="1"
-export FOX_USE_SED_BINARY="1"
-export FOX_USE_XZ_UTILS="1"
-export FOX_REPLACE_BUSYBOX_PS="1"
-export FOX_DELETE_INITD_ADDON="1" # !- Causes bootloops sometimes -!
-export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"; # Sun 21 Mar 04:26:40 GMT 2021
-export OF_USE_SYSTEM_FINGERPRINT="1"
-export FOX_DELETE_AROMAFM="1"
+	# Fox-specific flags
+	export OF_USE_GREEN_LED=0
+	export FOX_RESET_SETTINGS=1
+	export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
+	export FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER=1
+	export OF_NO_TREBLE_COMPATIBILITY_CHECK=1
+	export FOX_USE_BASH_SHELL=1
+	export FOX_ASH_IS_BASH=1
+	export FOX_USE_NANO_EDITOR=1
+	export FOX_USE_TAR_BINARY=1
+	export FOX_USE_ZIP_BINARY=1
+	export FOX_USE_SED_BINARY=1
+	export FOX_USE_XZ_UTILS=1
+	export FOX_REPLACE_BUSYBOX_PS=1
 
-#Backup
-export OF_QUICK_BACKUP_LIST="/boot;/data;/system_image;/vendor_image;"
+	export FOX_BUGGED_AOSP_ARB_WORKAROUND="1616300800"; # Sun 21 Mar 04:26:40 GMT 2021
+	export OF_USE_SYSTEM_FINGERPRINT=1
 
-# LZMA compression - this is needed!
-export FOX_USE_LZMA_COMPRESSION="1"
+	#Addons
+	export FOX_DELETE_INITD_ADDON=1 # !- Causes bootloops sometimes -!	
+	export FOX_DELETE_AROMAFM=1
 
-#Magiskboot
-export OF_USE_MAGISKBOOT="1"
-export OF_USE_NEW_MAGISKBOOT="1"
-export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES="1"
+	# LZMA compression - this is needed!
+	export FOX_USE_LZMA_COMPRESSION=1
 
-# OTA & MIUI
-export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION="1"
-export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES="1"
-export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR="1"
-export OF_DISABLE_MIUI_OTA_BY_DEFAULT="1"
-export OF_NO_MIUI_PATCH_WARNING="1"
+	#Magiskboot
+	export OF_USE_MAGISKBOOT=1
+	export OF_USE_NEW_MAGISKBOOT=1
+	export OF_USE_MAGISKBOOT_FOR_ALL_PATCHES=1
 
-# screen settings
-export OF_SCREEN_H="2160" #2340
-export OF_STATUS_H="76" #80
-export OF_STATUS_INDENT_LEFT="62" #48
-export OF_STATUS_INDENT_RIGHT="62" #48
-#export OF_HIDE_NOTCH=1
+	# OTA & MIUI
+	export OF_KEEP_DM_VERITY_FORCED_ENCRYPTION=1
+	export OF_SUPPORT_ALL_BLOCK_OTA_UPDATES=1
+	export OF_FIX_OTA_UPDATE_MANUAL_FLASH_ERROR=1
+	export OF_DISABLE_MIUI_OTA_BY_DEFAULT=1
+	export OF_NO_MIUI_PATCH_WARNING=1
 
-# R11 Settings
-export FOX_R11="1"
-export FOX_VERSION="R11.1"
-export FOX_VARIANT="A12L"
-export OF_MAINTAINER="melles1991"
-export OF_USE_TWRP_SAR_DETECT="1"
+	# screen settings
+	export OF_SCREEN_H=2160 #2340
+	export OF_STATUS_H=76 #80
+	export OF_STATUS_INDENT_LEFT=62 #48
+	export OF_STATUS_INDENT_RIGHT=62 #48
+	export OF_NO_SPLASH_CHANGE=1
 
-# Redmi 7/Y3 specific
-export FOX_BUILD_TYPE="RELEASE-UNOFFICIAL"
+    # -- add settings for R11 --
+	export FOX_R11=1
+	export OF_QUICK_BACKUP_LIST="/boot;/dtbo;/data;/system_image;/vendor_image;"
+	# -- end R11 settings --
 
-# let's see what are our build VARs
-if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
-  	export | grep "FOX" >> $FOX_BUILD_LOG_FILE
-  	export | grep "OF_" >> $FOX_BUILD_LOG_FILE
-   	export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
-  	export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+
+	# Redmi 7/Y3 specific
+	export FOX_BUILD_TYPE="Unofficial"
+	export FOX_VERSION=R11.1_1
+	export FOX_VARIANT=A12L
+	export OF_MAINTAINER=melles1991
+	export OF_USE_TWRP_SAR_DETECT=1
+
+	# let's see what are our build VARs
+	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
+  	   export | grep "FOX" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "OF_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TW_" >> $FOX_BUILD_LOG_FILE
+  	   export | grep "TARGET_" >> $FOX_BUILD_LOG_FILE
+ 	fi
+  	
 fi
